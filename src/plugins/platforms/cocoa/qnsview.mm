eff8ad4124d53d3226a3d4d941af80e19391ec08
Aditya Kolachana <akolachana@broadsoft.com>
2016-05-27 15:50:47 +0300

    BTUC-11641 Add Support for Services Menu
    Send QInputMethodQueryEvent to get selected text from the object
    which belongs to window with active focus.
    
    Change-Id: I7cb99fdb509c0c7d341af0218e2c6a0bc3128c5b

--- src/plugins/platforms/cocoa/qnsview.mm
+++ src/plugins/platforms/cocoa/qnsview.mm
@@ -129,6 +129,55 @@ static NSString *_q_NSWindowDidChangeOcclusionStateNotification = nil;
     NSString **notificationNameVar = (NSString **)dlsym(RTLD_NEXT, "NSWindowDidChangeOcclusionStateNotification");
     if (notificationNameVar)
         _q_NSWindowDidChangeOcclusionStateNotification = *notificationNameVar;
+
+
+    static BOOL initialized = NO;
+    /* Make sure code only gets executed once. */
+    if (initialized == YES) return;
+    initialized = YES;
+
+    [NSApp registerServicesMenuSendTypes:[NSArray arrayWithObjects:NSStringPboardType,
+                    NSRTFPboardType, nil] returnTypes:[NSArray arrayWithObjects:NSStringPboardType,
+                    nil]];
+}
+
+- (id)validRequestorForSendType:(NSString *)sendType
+            returnType:(NSString *)returnType
+{
+    if ([sendType isEqual:NSStringPboardType] && ![self selectionIsEmpty] ) {
+            return self;
+    }
+    return [super validRequestorForSendType:sendType returnType:returnType];
+}
+
+- (BOOL)writeSelectionToPasteboard:(NSPasteboard *)pboard
+types:(NSArray *)types
+{
+     NSArray *typesDeclared;
+
+    if ([types containsObject:NSStringPboardType] == NO) {
+        return NO;
+    }
+    typesDeclared = [NSArray arrayWithObject:NSStringPboardType];
+    [pboard declareTypes:typesDeclared owner:nil];
+    return [pboard setString:[self selection]
+                    forType:NSStringPboardType];
+}
+
+- (BOOL) selectionIsEmpty
+{
+    NSString* string = [self selection];
+    return (!string || [string length] == 0) ? YES : NO;
+}
+
+- (NSString*) selection
+{
+    NSAttributedString* sel = nil;
+    NSRange selRange = [self selectedRange];
+    if (selRange.length != 0)
+        sel = [self attributedSubstringForProposedRange:selRange actualRange:nil];
+
+	return sel.string;
 }
 
 - (id) init
@@ -1719,10 +1768,8 @@ static QTabletEvent::TabletDevice wacomTabletDevice(NSEvent *theEvent)
     QObject *fo = QGuiApplication::focusObject();
     if (!fo)
         return nil;
-    QInputMethodQueryEvent queryEvent(Qt::ImEnabled | Qt::ImCurrentSelection);
+    QInputMethodQueryEvent queryEvent( Qt::ImCurrentSelection);
     if (!QCoreApplication::sendEvent(fo, &queryEvent))
-        return nil;
-    if (!queryEvent.value(Qt::ImEnabled).toBool())
         return nil;
 
     QString selectedText = queryEvent.value(Qt::ImCurrentSelection).toString();
@@ -1756,10 +1803,8 @@ static QTabletEvent::TabletDevice wacomTabletDevice(NSEvent *theEvent)
     QObject *fo = QGuiApplication::focusObject();
     if (!fo)
         return selectedRange;
-    QInputMethodQueryEvent queryEvent(Qt::ImEnabled | Qt::ImCurrentSelection);
+    QInputMethodQueryEvent queryEvent( Qt::ImCurrentSelection);
     if (!QCoreApplication::sendEvent(fo, &queryEvent))
-        return selectedRange;
-    if (!queryEvent.value(Qt::ImEnabled).toBool())
         return selectedRange;
 
     QString selectedText = queryEvent.value(Qt::ImCurrentSelection).toString();
